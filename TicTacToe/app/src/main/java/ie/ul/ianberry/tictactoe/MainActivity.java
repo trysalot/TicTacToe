33f94c63fc43800496c2ffdda2ab958a370d18cf
trysalot <16230833@studentmail.ul.ie>
2018-10-03 18:06:02 +0100

    Starting

--- TicTacToe/app/src/main/java/ie/ul/ianberry/tictactoe/MainActivity.java
+++ TicTacToe/app/src/main/java/ie/ul/ianberry/tictactoe/MainActivity.java
@@ -3,11 +3,16 @@ package ie.ul.ianberry.tictactoe;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Set;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        // Place code after this line
+
+    }//end of main
+}//end of class
